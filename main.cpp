6925783272ef634d254b32dc9d3b116ad5c21f69
iceForge <15101009658@163.com>
2021-12-28 22:07:39 +0800

    Add log at startup.

--- main.cpp
+++ main.cpp
@@ -1,27 +1,19 @@
 #include <sys/resource.h>
 
-#include <iostream>
-#include <vector>
-#include <cassert>
-
 #include "boost/asio.hpp"
 #include "dtls_context.h"
-#include "hmac_sha1.h"
 #include "room_manager.h"
 #include "server_config.h"
 #include "signaling_server.h"
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
-#include "srtp_session.h"
 #include "threads.h"
-#include "webrtc_stream.h"
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
-    std::cout << "Parameter error. Usage: WebrtcSFU [Configuration file path]." << std::endl;
+    spdlog::error("Parameter error. Usage: WebrtcSFU [Configuration file path].");
     return EXIT_FAILURE;
   }
-  // TODO: More detailed log configuration.
 #ifdef NDEBUG
   spdlog::set_level(spdlog::level::info);
 #else
@@ -34,28 +26,29 @@ int main(int argc, char* argv[]) {
   setrlimit(RLIMIT_CORE, &l);
 
   // Test room with id 9527.
-  // TODO: If not used, remove this test code.
   RoomManager::GetInstance().CreateRoom("9527");
 
   if (!ServerConfig::GetInstance().Load(argv[1])) {
     spdlog::error("Failed to load configuration file.");
     return EXIT_FAILURE;
   }
+  spdlog::info("Configuration file loaded successfully.");
 
   if (!DtlsContext::GetInstance().Initialize()) {
     spdlog::error("Failed to initialize DTLS.");
     return EXIT_FAILURE;
   }
 
-  // TODO: If there is no certificate, use a self-signed certificate?
   if (!SignalingServer::GetInstance().LoadCertKeyFile(ServerConfig::GetInstance().GetCertFile(), ServerConfig::GetInstance().GetKeyFile())) {
     spdlog::error("Load file failed.");
     return EXIT_FAILURE;
   }
+
   if (!SignalingServer::GetInstance().Start(ServerConfig::GetInstance().GetIp(), ServerConfig::GetInstance().GetSignalingServerPort())) {
     spdlog::error("Signaling server failed to start.");
     return EXIT_FAILURE;
   }
+  spdlog::info("Signaling service listening {} port.", ServerConfig::GetInstance().GetSignalingServerPort());
 
   boost::asio::signal_set signals(MainThread::GetInstance().MessageLoop(), SIGINT, SIGTERM);
   signals.async_wait([&](const boost::system::error_code &error, int signal_number) {
@@ -66,6 +59,8 @@ int main(int argc, char* argv[]) {
       WorkerThreadPool::GetInstance().StopAll();
     }
   });
+  spdlog::info("Service started successfully");
+
   MainThread::GetInstance().MessageLoop().run();
   return EXIT_SUCCESS;
 }
