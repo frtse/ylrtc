b86341d52ad15f5e4cffb3440a426de657a6c966
iceForge <15101009658@163.com>
2022-01-06 17:27:32 +0800

    Transfer the publish method to the client object.

--- webclient/js/client.js
+++ webclient/js/client.js
@@ -29,6 +29,62 @@ class Client extends EventDispatcher {
     return res.roomInfo;
   }
 
+  /**
+   * Publish stream.
+   * @param {*} device Device
+   * @param {*} videoRtpEncodingParameters RTCRtpEncodingParameters https://developer.mozilla.org/en-US/docs/Web/API/RTCRtpEncodingParameters
+   * @param {*} audioRtpEncodingParameters RTCRtpEncodingParameters https://developer.mozilla.org/en-US/docs/Web/API/RTCRtpEncodingParameters
+   */
+  async publish(device, audioRtpEncodingParameters, videoRtpEncodingParameters) {
+    let mediaStream = device.mediaStream();
+    const configuration = {bundlePolicy: "max-bundle"};
+    let pc = new RTCPeerConnection(configuration);
+
+    const audioTransceiverInit = {
+      direction: 'sendonly',
+      sendEncodings: [{rid: 'q', active: true, maxBitrate: 64000}],
+      streams: [mediaStream]
+    };
+
+    if (audioRtpEncodingParameters !== null)
+      audioTransceiverInit.sendEncodings = audioRtpEncodingParameters;
+
+    const videoTransceiverInit = {
+      direction: 'sendonly',
+      sendEncodings: [{rid: 'q', active: true, maxBitrate: 200000}],
+      streams: [mediaStream]
+    };
+
+    if (videoRtpEncodingParameters !== null)
+      videoTransceiverInit.sendEncodings = videoRtpEncodingParameters;
+
+    if (mediaStream.getAudioTracks().length >
+      0) {
+      pc.addTransceiver(
+        mediaStream.getAudioTracks()[0],
+        audioTransceiverInit);
+    }
+    if (mediaStream.getVideoTracks().length >
+      0) {
+      pc.addTransceiver(
+        mediaStream.getVideoTracks()[0],
+        videoTransceiverInit);
+    }
+
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+    var request = { action: "publish", offer: offer.sdp };
+    let res = await this.signaling.sendRequest(request);
+    if (res.error)
+      throw "Publish failed.";
+    var answerSdp = new RTCSessionDescription();
+    answerSdp.sdp = res.answer;
+    answerSdp.type = 'answer';
+    let streamId = res.streamId;
+    await pc.setRemoteDescription(answerSdp);
+    return new PublisheStream(mediaStream, streamId, pc, this.signaling);
+  }
+
   leave() {
     if (this.joined_) {
       this.joined_ = false;
--- webclient/js/publisheStream.js
+++ webclient/js/publisheStream.js
@@ -1,68 +1,13 @@
 class PublisheStream {
-  constructor(signaling) {
-    this.mediaStream_ = null;
-    this.streamId_ = '';
-    this.pc_ = null;
+  constructor(mediaStream, streamId, pc, signaling) {
+    this.mediaStream_ = mediaStream;
+    this.streamId_ = streamId;
+    this.pc_ = pc;
     this.signaling_ = signaling;
   }
 
   Id() {
     return this.streamId_;
-  }
-
-  /**
-   * Publish stream.
-   * @param {*} device Device
-   * @param {*} videoRtpEncodingParameters RTCRtpEncodingParameters https://developer.mozilla.org/en-US/docs/Web/API/RTCRtpEncodingParameters
-   * @param {*} audioRtpEncodingParameters RTCRtpEncodingParameters https://developer.mozilla.org/en-US/docs/Web/API/RTCRtpEncodingParameters
-   */
-  async publish(device, audioRtpEncodingParameters, videoRtpEncodingParameters) {
-    this.mediaStream_ = device.mediaStream();
-    const configuration = {bundlePolicy: "max-bundle"};
-    this.pc_ = new RTCPeerConnection(configuration);
-
-    const audioTransceiverInit = {
-      direction: 'sendonly',
-      sendEncodings: [{rid: 'q', active: true, maxBitrate: 64000}],
-      streams: [this.mediaStream_]
-    };
-
-    if (audioRtpEncodingParameters !== null)
-      audioTransceiverInit.sendEncodings = audioRtpEncodingParameters;
-
-    const videoTransceiverInit = {
-      direction: 'sendonly',
-      sendEncodings: [{rid: 'q', active: true, maxBitrate: 200000}],
-      streams: [this.mediaStream_]
-    };
-
-    if (videoRtpEncodingParameters !== null)
-      videoTransceiverInit.sendEncodings = videoRtpEncodingParameters;
-
-    if (this.mediaStream_.getAudioTracks().length >
-      0) {
-      this.pc_.addTransceiver(
-        this.mediaStream_.getAudioTracks()[0],
-        audioTransceiverInit);
-    }
-    if (this.mediaStream_.getVideoTracks().length >
-      0) {
-      this.pc_.addTransceiver(
-        this.mediaStream_.getVideoTracks()[0],
-        videoTransceiverInit);
-    }
-
-    const offer = await this.pc_.createOffer();
-    await this.pc_.setLocalDescription(offer);
-    var request = { action: "publish", offer: offer.sdp };
-    let res = await this.signaling_.sendRequest(request);
-    if (res.error)
-      throw "Publish failed.";
-    var answerSdp = new RTCSessionDescription();
-    answerSdp.sdp = res.answer;
-    answerSdp.type = 'answer';
-    this.streamId_ = res.streamId;
-    await this.pc_.setRemoteDescription(answerSdp);
   }
 
   unpublish() {
